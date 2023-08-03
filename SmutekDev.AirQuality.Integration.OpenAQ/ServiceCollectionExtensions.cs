37bde5d7b855c14e27434b3f032661c5eaa293da
Damian Smutek <damian.smutek@firstlinesoftware.com>
2023-08-03 09:59:11 +0000

    Add OpenAQ integration project

---
+++ SmutekDev.AirQuality.Integration.OpenAQ/ServiceCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+namespace SmutekDev.AirQuality.Integration.OpenAQ;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddOpenAQ(this IServiceCollection services)
+    {
+        return services;
+    }
+}
